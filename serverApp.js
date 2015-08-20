7ab06473126b585b92d49396b27967e905827b02
kyn100 <kyn100@gmail.com>
2015-08-19 20:55:39 -0400

    init

---
+++ serverApp.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/sp500', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+var Schema = mongoose.Schema;
+var pwdSchema = new Schema( {
+    name: String,
+    value: String
+});
+
+// Mongoose Model definition
+var SP500 = mongoose.model('SP500', pwdSchema, 'sp500');
+//app.get('/', function (req, res) {
+//    res.send('Hello World');
+//});
+app.get('/sp500', function (req, res) {
+    SP500.find({ 'etf': 'true' }, function (err, docs) {
+        if (err) return console.error(err);
+        res.json(docs);
+    });
+
+});
+
+app.listen(3000);
